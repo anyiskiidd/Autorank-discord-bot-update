1ed6e818e1ffc7abca7d4d23fc99ef405137a058
F4llen-Sys <131175492+F4llenSys@users.noreply.github.com>
2023-05-06 16:15:18 +0200

    Update index.py
    autorank v2

--- index.py
+++ index.py
@@ -1,40 +1,75 @@
 import discord
 from discord.ext import commands
+from datetime import datetime
 
 intents = discord.Intents.default()
 intents.members = True
 
-client = commands.Bot(command_prefix='!', intents=intents) ##Et ça c'est les intents pour dire que le bot dispose de la permission
+client = commands.Bot(command_prefix='!', intents=intents)
 
-ROLE_ID = "id du role que le bot va donner"
-CHANNEL_ID = "id du channel spécifique dans lequel le membre devra mentionner le bot"
+ROLE_ID = "id du role"
+CHANNEL_ID = "id du channel dans lequel vous voulez que le bot sois mentionné"
+LOG_CHANNEL_ID = "id dans lequel vous voulez que les logs appraisses"
+
 
 @client.event
 async def on_ready():
     print("Bot prêt")
 
+
+@client.event
+async def on_member_update(before, after):
+    role = after.guild.get_role(ROLE_ID)
+    if role is not None:
+        if ("tag1" in before.name or "tag2" in before.name) and not ("tag1" in after.name or "tag2" in after.name):
+            if role in before.roles:
+                await after.remove_roles(role)
+                message = f"{after.mention} a perdu le rôle {role.name}"
+                print(message)
+        elif ("tag1" not in after.name and "tag2" not in after.name):
+            if role in after.roles:
+                await after.remove_roles(role)
+                message = f"{after.mention} a perdu le rôle {role.name}"
+                print(message)
+
+
 @client.event
 async def on_message(message):
-    if not message.author.bot and client.user.mentioned_in(message):
+    if not message.author.bot and client.user in message.mentions:
         member = message.author
-        if "tag" or "tag" in member.name: ##"If" c'est pour dire que si il a tag=211 ou tag=²¹¹ la commande marche, et le "else" c'est pour le cas contraire
-            if message.channel.id == CHANNEL_ID and not ("@everyone" in message.content or "@here" in message.content): ##Pour que ca ne prenne pas en compte @everyone et @here
-                role = message.guild.get_role(ROLE_ID)
+        if "211" in member.name or "²¹¹" in member.name:
+            if message.channel.id == CHANNEL_ID and not ("@everyone" in message.content or "@here" in message.content):
+                role = message.guild.get_role(1086755223525130281) # Récupère le rôle avec l'ID "1086755223525130281"
                 if role is not None:
                     await member.add_roles(role)
-                    reply_msg = f"{member.mention}, le rôle vous a été donné avec succès !"
+                    reply_msg = f'{member.mention}, Le rôle "{role.name}" vous a été donné avec succès !'
+                    print(reply_msg)
+                    
+                    log_embed = discord.Embed(title=f"Nouveau Log Autorank !", color=discord.Color.from_rgb(25, 25, 25))
+                    log_embed.add_field(name="Modérateur", value=f"<@(id de votre bot)>")
+                    log_embed.add_field(name="Membre", value=f"{member.mention}")
+                    log_embed.add_field(name="Rôle donné", value=f"{role.mention}")
+                    log_embed.set_footer(text=f"Donné le {datetime.utcnow().strftime('à %Y-%m-%d %H:%M:%S')}")
+                    
+                    log_channel = client.get_channel(LOG_CHANNEL_ID)
+                    if log_channel is not None:
+                        await log_channel.send(embed=log_embed)
                 else:
-                    reply_msg = f"Le rôle est introuvable sur ce serveur."
+                    reply_msg = f"Une erreur est survenue, veuillez contacter un admin !"
             else:
-                reply_msg = f"{member.mention}, vous devez mentionner le bot dans le canal {client.get_channel(CHANNEL_ID).mention} pour recevoir le rôle !" ##{client.get_channel(CHANNEL_ID).mention} c'est pour que ca mentionne le channel défini par "CHANNEL_ID = "channel id""
+                reply_msg = f"{member.mention}, vous devez mentionner le bot dans le canal {client.get_channel(CHANNEL_ID).mention} pour recevoir le rôle !"
         else:
-            reply_msg = f"{member.mention}, tu n'as pas le bon pseudo pour recevoir le rôle."
+            role = message.guild.get_role(ROLE_ID)
+            if role is not None and role in member.roles:
+                await member.remove_roles(role)
+                reply_msg = f"{member.mention}, Vous avez perdu le rôle {role.name} car vous n'avez plus le tag !"
+                print(reply_msg)
+            else:
+                reply_msg = f"{member.mention}, Vous devez avoir le tag pour obtenir le rôle !"
+
+        bot_message = await message.channel.send(reply_msg)
         
-        bot_message = await message.reply(reply_msg) ##Ca c'est pour que le bot reponde au mess
-        await bot_message.delete(delay=7) ##Ca ca supprime les mess du bot après 7sec
-    
     await client.process_commands(message)
 
-client.run('token')
 
-##Je t'ai mit toute les explications comme ca si tu veux modif le script tu peux
+client.run('token')
